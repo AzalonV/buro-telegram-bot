0025c30e4ad64ae4349248b6b881388c3c80440a
AzalonV <95941461+AzalonV@users.noreply.github.com>
2024-08-06 02:21:28 +0300

    Update scheduler_complete.py

--- scheduler_complete.py
+++ scheduler_complete.py
@@ -8,4 +8,5 @@ async def scheduler_start():
     scheduler = AsyncIOScheduler(timezone='Europe/Kiev')
     scheduler.add_job(ScheduleService.delet_unsuitable_event, 'cron', hour=1)
     scheduler.add_job(ScheduleService.delet_unsuitable_message, 'cron', hour=1)
+    scheduler.add_job(ScheduleService.keep_alive, 'interval', seconds=3)
     scheduler.start()
