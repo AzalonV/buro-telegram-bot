9688cb277ce7e3501bf80961d2ff0a44f6c8aa9d
AzalonV <95941461+AzalonV@users.noreply.github.com>
2024-08-06 10:29:44 +0300

    Update start.py

--- start.py
+++ start.py
@@ -5,11 +5,16 @@ import sys
 from bot import main
 from scheduler_complete import scheduler_start
 from database.sql import create_db_and_tables
-
+from hypercorn.asyncio import serve
+from hypercorn.config import Config
+from flask import app
 
 
 async def start() -> None:
     
+    config = Config()
+    config.bind = ["0.0.0.0:5000"]
+    await serve(app, config)
     await scheduler_start()
     await create_db_and_tables()
     await main()
