7891e7a467c7d0ff789017e331bc97406ef2ec1c
AzalonV <95941461+AzalonV@users.noreply.github.com>
2024-08-06 10:43:57 +0300

    Update start.py

--- start.py
+++ start.py
@@ -7,7 +7,7 @@ from scheduler_complete import scheduler_start
 from database.sql import create_db_and_tables
 from hypercorn.asyncio import serve
 from hypercorn.config import Config
-from flask.flask import app
+from magic import app
 
 
 async def start() -> None:
